d5cfd47879265f1ffeb47c25dd439c070a3677d1
Thanu Krishna <59538125+thanukrishna8@users.noreply.github.com>
2020-03-04 23:03:51 +0000

    Add files via upload

---
+++ fizzbuzz/src/main/java/com/spartaglobal/fizz/Main.java
@@ -0,0 +1,18 @@
+package com.spartaglobal.fizz;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
+        System.out.println(fizzBuzzGenerator.divisibleBy(4, 2));
+        System.out.println(fizzBuzzGenerator.divisibleBy(3, 2));
+        System.out.println(fizzBuzzGenerator.FizzBuzz(1, 15).toString());
+    }
+}
